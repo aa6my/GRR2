b4f151026c3df6f6944568d99674a6ea83a018db
shirousky <harizafif.nasry@gmail.com>
2015-02-23 01:58:38 +0800

    edit icon top

--- application/views/header.php
+++ application/views/header.php
@@ -28,7 +28,7 @@
 
 	<!-- Favicons
 	================================================== -->
-	<link rel="shortcut icon" href="<?php echo base_url() ?>assets/images/favicon.ico">
+	<link rel="shortcut icon" href="<?php echo base_url() ?>assets/images/grr.png">
 	<link rel="apple-touch-icon" href="<?php echo base_url() ?>assets/images/apple-touch-icon.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url() ?>assets/images/apple-touch-icon-72x72.png">
 	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url() ?>assets/images/apple-touch-icon-114x114.png">
